11777849677972bc87be29bea460da38b9e09611
Weijian Yang <ywj200844@gmail.com>
2014-05-03 15:28:55 +0800

    complete cooperationArea page

---
+++ static/js/fixImgSize.js
@@ -0,0 +1,28 @@
+$(function(){
+	$(document).ready(function(){
+		(function(){
+			
+			window.fixImgSize = function(imgObj){
+				var imgWidth        = imgObj.width;
+				var imgHeight       = imgObj.height;
+				var parentNode      = imgObj.parentNode;
+				var parentWidth     = $(parentNode).width();
+				var parentHeight    = $(parentNode).height();
+				var changeSizeJudge = imgWidth*parentHeight/(imgHeight*parentWidth);
+				if(changeSizeJudge > 1){
+					var changeImgHeight = imgHeight*parentWidth/imgWidth;
+					$(imgObj).width(parentWidth);
+					$(imgObj).height(changeImgHeight);
+					$(imgObj).css("margin-top",(parentHeight - changeImgHeight)/2.0)
+					$(imgObj).css("display","inline-block");
+					//console.log(imgObj,"wdith")
+				}
+				else{
+					$(imgObj).height(parentHeight);
+					$(imgObj).width(imgWidth*parentHeight/imgHeight);
+					$(imgObj).css("display","inline-block");
+				}
+			}
+		})();
+	});
+});
